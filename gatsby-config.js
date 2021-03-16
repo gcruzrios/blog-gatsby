4d26270d86a333a3ab3708f24086bd9067d28f3f
gcruzrios <gcruzr@gmail.com>
2021-03-15 18:24:09 -0600

    Primera version de Gatsby Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Greivin.net`,
+    description: `Blog sobre programación en Javascript React & Gatsby.`,
+    author: `@gcruzrios`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,21 +16,36 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-source-strapi`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`post`],
+        //If using single types place them in this array.
+        //singleTypes: [`home-page`, `contact`],
+        // loginData: {
+        //   identifier: "gcruzrios",
+        //   password: "Grvn240675$$",
+        // },
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    //{
+      // resolve: `gatsby-plugin-manifest`,
+      // options: {
+      //   name: `gatsby-starter-default`,
+      //   short_name: `starter`,
+      //   start_url: `/`,
+      //   background_color: `#663399`,
+      //   theme_color: `#663399`,
+      //   display: `minimal-ui`,
+      //   icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      // },
+    //},
+
+    //`gatsby-plugin-gatsby-cloud`,
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
